36d6c9c7e0355ff67a66d4550b3928e934ee636e
Patrick Bornay <patrick.bornay@protonmail.com>
2019-05-05 12:25:12 +1000

    modified main to take cli arguments

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+
+int main(int argc, char* argv[]){
+    std::cout << "Charging Partical Accellerators.." << '\n';
+    std::cout << '\n';
+    std::cout << "Lauching The Space Journey" << '\n';
+    //take cli args
+    const int limit = std::atoi(argv[1]);
+    std::cout << "Targeting " << limit << " sectors from initializtion point" << '\n';
+
+    //initialize spaceship
+
+    //initialize officiers and skill stats
+
+    //
+
+    return 0;
+}
